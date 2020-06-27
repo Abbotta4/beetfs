878a52394746b979bcc859f8733f63c7eb25b37a
Drew Abbott <abbotta4@gmail.com>
2020-06-27 16:17:07 -0400

    Add fs node class, readdir, and .gitignore entries
    Add filesystem tree node class and helper functions
    Add basic implementation of readdir
    Change pass function fillers to returning -ENOSYS
    Change naming scheme for classes to UpperCase
    Add venv content, beets logs and config, and beetsplug dir to .gitignore

--- beetsplug/beetfs.py
+++ beetsplug/beetfs.py
@@ -5,6 +5,7 @@ try:
 except ImportError:
     pass
 import fuse
+import errno
 from fuse import Fuse
 from beets import config as Config
 from beets.plugins import BeetsPlugin
@@ -15,21 +16,63 @@ PATH_FORMAT_SPLIT = PATH_FORMAT.split('/')
 
 mount_command = Subcommand('mount', help='mount a beets filesystem')
 def mount(lib, opts, args):
+    global fs_tree
+    fs_tree = build_fs_tree(lib)
+    """
+    for f in fs_tree.children:
+        print(f.name)
+        for g in f.children:
+            print(g.name)
+            for h in g.children:
+                print(h.name)
     print("PATH_FORMAT = ", PATH_FORMAT)
     """
     usage = "where and when will this print?"
-    server = beet_fuse(version="%prog " + fuse.__version__,
+    server = BeetFuse(version="%prog " + fuse.__version__,
                      usage=usage,
                      dash_s_do='setsingle')
     server.parse(errex=1)
     server.main()
-    """
 mount_command.func = mount
 
-class beetfs(BeetsPlugin):
+def build_fs_tree(library):
+    items = list(library.items())
+    root = TreeNode()
+    height = len(PATH_FORMAT_SPLIT)
+    for item in items:
+        cursor = root
+        for depth in range(0, height):
+            name = item.evaluate_template(PATH_FORMAT_SPLIT[depth])
+            child = TreeNode(name, cursor)
+            cursor = cursor.add_child(child)
+    return root
+
+class BeetFs(BeetsPlugin):
     def commands(self):
         return [mount_command]
 
+class TreeNode():
+    def __init__(self, name='root', parent=None):
+        self.name = name
+        self.parent = parent
+        self.children = []
+
+    def add_child(self, child):
+        for _child in self.children:
+            if _child.name == child.name:
+                return _child
+        else:
+            self.children.append(child)
+            return child
+
+    def find(self, target): # linear search :(
+        if self.name == target.name:
+            return self
+        for child in self.children:
+            result = child.find(target)
+            if result:
+                return result
+        
 if not hasattr(fuse, '__version__'):
     raise RuntimeError("your fuse-py doesn't know of fuse.__version__, probably it's too old.")
 
@@ -51,15 +94,25 @@ class Stat(fuse.Stat):
         self.st_mtime = 0
         self.st_ctime = 0
 
-class beet_fuse(Fuse):
+class BeetFuse(Fuse):
     def getattr(self, path):
-        pass
+        return -errno.ENOSYS
 
     def readdir(self, path, offset):
-        pass
+        path_split = path.split('/')[1:]
+        height = len(path_split)
+        root = fs_tree
+        if path == '/':
+            dirs = root
+        else:
+            for depth in range(0,height):
+                root = root.find(path_split[depth])
+            dirs = root
+        for d in  ('.', '..', *(x.name for x in dirs.children)):
+            yield fuse.Direntry(d)
 
     def open(self, path, flags):
-        pass
+        return -errno.ENOSYS
 
     def read(self, path, size, offset):
-        pass
+        return -errno.ENOSYS
